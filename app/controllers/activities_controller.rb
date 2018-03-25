d8e16d0da17e757fe8805cd03c0dc9b548135bbc
Belal Kamara <belalkamara@gmail.com>
2018-03-25 11:41:50 -0500

    Integrated new and create action for the activity feature

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,22 @@ class ActivitiesController < ApplicationController
   def index
     @activity_events = Activity.all
   end
+
+  def new
+    @activity_events = Activity.new
+  end
+
+  def create
+    @activity_events = Activity.new(params.require(:activity).permit(:title, :miles, :image))
+
+    respond_to do |format|
+      if @activity_events.save
+        format.html { redirect_to activities_path, notice: 'Your event was successfully created.' }
+        format.json { render :show, status: :created, location: @activity_events }
+      else
+        format.html { render :new }
+        format.json { render json: @activity_events.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
