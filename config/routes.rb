ef5389294ee2f4fe0ae217c70fb5384e8785f420
Belal Kamara <belalkamara@gmail.com>
2018-04-02 21:07:49 -0500

    Made a custom activity routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :activities
+  resources :activities, except: [:show]
+  get 'activity/:id', to: 'activities#show', as: "activity_show"
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
