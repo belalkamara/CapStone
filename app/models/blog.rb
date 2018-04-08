47e572c2c7de48368b5046250353a043a7988685
Belal Kamara <belalkamara@gmail.com>
2018-04-08 11:31:20 -0500

    Implemented a default image for blogs and default topic for nil blogs.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,7 +3,14 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  validates_presence_of :title, :body
+  validates_presence_of :title, :body, :picture, :topic_id
 
   belongs_to :topic
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.picture ||= "http://via.placeholder.com/400x300"
+    self.topic_id ||= Topic.last.id
+  end
 end
