9bdff86799b093b210a0aa194db64919089b6009
Belal Kamara <belalkamara@gmail.com>
2018-04-10 19:47:18 -0500

    Fixed bug with ended status in activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -14,16 +14,15 @@ class Activity < ApplicationRecord
     where(status: 'live')
   end
 
-  after_initialize :set_defaults, :set_status
+  after_initialize :set_defaults
+  after_update :set_status
 
   def set_defaults
     self.image ||= ImagePlaceholder.image_generator(height:'300', width:'200')
   end
 
   def set_status
-    if self.days.nil? || self.days == 0
-      self.ended!
-    end
+    self.update_column(:status, 2) if self.days == 0
   end
 end
 
