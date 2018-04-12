c1aa9cf343d13a194fdbb472ea6eaae4214b81e2
Belal Kamara <belalkamara@gmail.com>
2018-04-11 20:25:11 -0500

    Added name parameters for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :config_auth, if: :devise_controller?
+
+  def config_auth
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
