03e6178ae51625f9a8f0da3b63ed50794e9dd78b
Belal Kamara <belalkamara@gmail.com>
2018-04-12 20:40:30 -0500

    Refactored the devise whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :config_auth, if: :devise_controller?
-
-  def config_auth
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist
 end
