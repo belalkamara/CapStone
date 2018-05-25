da09b6a65242760d88bf6f1d34d53b572301056a
Belal Kamara <belalkamara@gmail.com>
2018-05-24 21:55:20 -0500

    Separated the users draft blog and published blogs in the dashboard

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -2,6 +2,10 @@ class UserDashboardController < ApplicationController
 
   def index
     @user_blogs = Blog.page(params[:page]).per(7).blogs_by current_user
+
+    @user_draft_blogs = Blog.draft.blogs_by current_user
+
+    @user_published_blogs = Blog.published.blogs_by current_user
   end
 
   def profile
