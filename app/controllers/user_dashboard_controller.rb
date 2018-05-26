176e957b925cb4d460037b892c53195966a56185
Belal Kamara <belalkamara@gmail.com>
2018-05-26 12:44:37 -0500

    Added the basic styles for profile in user dash

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -1,11 +1,11 @@
 class UserDashboardController < ApplicationController
 
   def index
-    @user_blogs = Blog.page(params[:page]).per(7).blogs_by current_user
-
     @user_draft_blogs = Blog.draft.blogs_by current_user
 
     @user_published_blogs = Blog.published.blogs_by current_user
+
+    # @user_miles = User.miles
   end
 
   def profile
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506164943) do
+ActiveRecord::Schema.define(version: 20180525030714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,8 @@ ActiveRecord::Schema.define(version: 20180506164943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "roles"
+    t.date "day"
+    t.integer "miles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
