464cdff9985113caac7be2fee76c49f42c162752
Belal Kamara <belalkamara@gmail.com>
2018-06-02 13:59:45 -0500

    Implemented carrier wave for activity file upload

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -11,6 +11,8 @@ class Activity < ApplicationRecord
 
   validates_presence_of :title, :miles, :image, :status, :description, :start_date, :end_date, :user_id
 
+  mount_uploader :image, ActivityUploader
+
   scope :activities_by, ->(user) { where(user_id: user.id) }
 
   def self.draft
