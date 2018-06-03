332797b430001d22f616ca39f2ad515ebdae8c1b
Belal Kamara <belalkamara@gmail.com>
2018-06-03 14:08:13 -0500

    Added the logo as a logo default if activity does not have image

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,13 +1,13 @@
 module ActivitiesHelper
-  def image_generator(height:, width:)
-    "http://via.placeholder.com/#{height}x#{width}"
+  def image_generator
+    "logo-rtd.png"
   end
 
   def act_img img, type
     if img.model.image?
       img
     else type == 'image'
-      image_generator(height:'300', width:'200')
+      image_generator
     end 
   end
 end
