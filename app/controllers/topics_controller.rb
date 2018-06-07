17862c56fa2a279bd5b7a9a25b52fe64b767b2f1
Belal Kamara <belalkamara@gmail.com>
2018-06-07 15:46:55 -0500

    Changed the sidebar to only show topics that have blogs associated to them

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,4 +1,5 @@
 class TopicsController < ApplicationController
+  before_action :set_sidebar_topics
   layout 'blog'
   def index
     @topics = Topic.all
@@ -13,5 +14,13 @@ class TopicsController < ApplicationController
       @blogs = @topic.blogs.published.recent.page(params[:page]).per(7)
     end
   end
+
+  private
+
+  def set_sidebar_topics
+    @sidebar_topics  = Topic.with_blogs
+  end
 end
+
+
 
