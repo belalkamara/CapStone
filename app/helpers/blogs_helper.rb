e99932c6f67d00d4660cdcb8790537fc4f0a8948
Belal Kamara <belalkamara@gmail.com>
2018-06-07 02:59:16 -0500

    Did initial styles for comments

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -10,4 +10,8 @@ module BlogsHelper
       picture_generator
     end 
   end
+
+  def gravatar_helper user
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
 end
